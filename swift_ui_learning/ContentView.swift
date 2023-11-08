db1fe8b6caac1d003c43e054afbff98339d4364f
Murad Azimov <kalmahik@yahoo.com>
2023-11-08 07:25:31 +0300

    Initial Commit

---
+++ swift_ui_learning/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift_ui_learning
+//
+//  Created by Murad Azimov on 08.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_ui_learning/swift_ui_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_learningApp.swift
+//  swift_ui_learning
+//
+//  Created by Murad Azimov on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
